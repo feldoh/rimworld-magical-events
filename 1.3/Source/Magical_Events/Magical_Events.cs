f9d6b1b144e138f0542f0590cd25257a5bf0261d
Dexter Lowe <feldoh10@gmail.com>
2022-05-25 00:23:03 +0100

    Cookiecutter

---
+++ 1.3/Source/Magical_Events/Magical_Events.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Linq;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Magical_Events
+{
+	public class Mod : Verse.Mod
+	{
+	public static Settings settings;
+		public Mod(ModContentPack content) : base(content)
+		{
+			Log.Message("Hello world from Magical Events");
+
+			// initialize settings
+			settings = GetSettings<Settings>();
+
+#if DEBUG
+			Harmony.DEBUG = true;
+#endif
+
+			Harmony harmony = new Harmony("Taggerung.rimworld.Magical_Events.main");	
+			harmony.PatchAll();
+		}
+
+		public override void DoSettingsWindowContents(Rect inRect)
+		{
+			base.DoSettingsWindowContents(inRect);
+			settings.DoWindowContents(inRect);
+		}
+
+		public override string SettingsCategory()
+		{
+			return "Magical Events";
+		}
+	}
+}
